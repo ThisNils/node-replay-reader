2491d30e69b658ff4a75f676b8329fdbd08af965
Jarvis Prestidge <jarvis@truelayer.com>
2021-04-06 23:49:38 +0100

    Re-add binary reader export

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-// Main lib export
+// Main lib exports
 export { default as ReplayReader } from './src/ReplayReader';
+export { default as BinaryReader } from './src/BinaryReader';
 
 // Supporting interfaces
 export * from './src/structs';
