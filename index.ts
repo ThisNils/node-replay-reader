34cf3744cbba973f8d4df853ce45407d3e058e10
ThisNils <schlinnils@gmail.com>
2021-12-07 13:57:40 +0100

    cleanup

--- index.ts
+++
@@ -1,6 +0,0 @@
-// Main lib exports
-export { default as ReplayReader } from './src/ReplayReader';
-export { default as BinaryReader } from './src/BinaryReader';
-
-// Supporting interfaces
-export * from './src/structs';
