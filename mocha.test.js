06ba74f0fbe3c30fa89fb8eb0818fc33352f0e69
ThisNils <59409978+ThisNils@users.noreply.github.com>
2022-08-12 01:58:24 +0200

    updated tests

--- mocha.test.js
+++ mocha.test.js
@@ -3,20 +3,13 @@ const assert = require('assert');
 const { ReplayReader } = require('.');
 
 describe('Parsing a replay', () => {
-  it('Should initialize the reader', () => {
-    // eslint-disable-next-line no-unused-vars
-    const reader = new ReplayReader('./examples/replays/1.replay');
-  });
-
   it('Should parse a replay', async () => {
-    const reader = new ReplayReader('./examples/replays/1.replay');
-    const replay = await reader.parse();
+    const replay = await ReplayReader.parse('./examples/replays/1.replay');
     assert.notStrictEqual(typeof replay.meta, 'undefined', 'The replay meta should be defined');
   });
 
   it('Should parse a replay with resolveAccountNames enabled', async () => {
-    const reader = new ReplayReader('./examples/replays/1.replay', { resolveAccountNames: true });
-    const replay = await reader.parse();
+    const replay = await ReplayReader.parse('./examples/replays/1.replay', { resolveAccountNames: true });
     assert.notStrictEqual(typeof replay.meta, 'undefined', 'The replay meta should be defined');
   });
 });
